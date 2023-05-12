fd9f1e7cee80966bd6b18953cdd3266d1aa9ecdd
bertazs <kicsibertazsolt@gmail.com>
2023-05-12 06:54:00 +0000

    favicon

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,6 +9,18 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="keywords" content="hulladékgazdálkodás, önkormányzati, társulás, Nyugat-Dunántúli Regionális Hulladékgazdálkodási Önkormányzati Társulás">
         <meta name="Description" content="hulladékgazdálkodás, önkormányzati, társulás, Nyugat-Dunántúli Regionális Hulladékgazdálkodási Önkormányzati Társulás">
+
+        {{-- favicon --}}
+        <link rel="apple-touch-icon" sizes="180x180" href="{{asset('/img/main/apple-touch-icon.png')}}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{asset('/img/main/favicon-32x32.png')}}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{asset('/img/main/favicon-16x16.png')}}">
+        <link rel="manifest" href="{{asset('/img/main/site.webmanifest')}}">
+        <link rel="mask-icon" href="{{asset('/img/main/safari-pinned-tab.svg')}}" color="#5bbad5">
+        <link rel="shortcut icon" href="{{asset('/img/main/favicon.ico')}}">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="msapplication-config" content="{{asset('/img/main/browserconfig.xml')}}">
+        <meta name="theme-color" content="#ffffff">
+
         {{-- title --}}
         <title>
             Nyugat-Dunántúli Regionális Hulladékgazdálkodási Önkormányzati
