606a2049e321cc176900891a667cefaa30575746
Dominik <dominik.krizsn@gmail.com>
2025-09-24 21:30:17 +0200

    dokumentumok

--- resources/views/guest/tarsulasrol/ulesek.blade.php
+++ resources/views/guest/tarsulasrol/ulesek.blade.php
@@ -41,6 +41,14 @@ background: url({{asset('./img/main/inner-bg.png')}});
                     </thead>
                     <tbody>
                         <tr class="bg-white border-b bg-zinc-800 border-gray-700 hover:bg-zinc-700 ease-in duration-100">
+                            <td class="px-6 py-4">2025.08.29</td>
+                            <td class="px-6 py-4"><a class="text-zinc-300 hover:text-zinc-400 duration-100" href="{{asset('./pdf/tarsulasrol/tarsulasulesek/2025/2025.08.29.zip')}}">link</a></td>
+                        </tr>
+                        <tr class="bg-white border-b bg-zinc-800 border-gray-700 hover:bg-zinc-700 ease-in duration-100">
+                            <td class="px-6 py-4">2025.08.22</td>
+                            <td class="px-6 py-4"><a class="text-zinc-300 hover:text-zinc-400 duration-100" href="{{asset('./pdf/tarsulasrol/tarsulasulesek/2025/2025.08.22.zip')}}">link</a></td>
+                        </tr>
+                        <tr class="bg-white border-b bg-zinc-800 border-gray-700 hover:bg-zinc-700 ease-in duration-100">
                             <td class="px-6 py-4">2025.06.30</td>
                             <td class="px-6 py-4"><a class="text-zinc-300 hover:text-zinc-400 duration-100" href="{{asset('./pdf/tarsulasrol/tarsulasulesek/2025/20250630.zip')}}">link</a></td>
                         </tr>
